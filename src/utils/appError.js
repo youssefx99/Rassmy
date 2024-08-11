f276d8dd930cd3731963d6399a638f928edc8771
Youssef Faisal <125581694+youssefx99@users.noreply.github.com>
2024-08-11 19:21:49 +0300

    Update appError.js

--- src/utils/appError.js
+++ src/utils/appError.js
@@ -1,5 +1,13 @@
 class AppError extends Error {
   constructor(statusCode, message) {
+    if (typeof statusCode !== "number" || typeof message !== "string") {
+      throw new Error("Invalid input types. Expected a number and a string.");
+    }
+
+    let temp = statusCode;
+    statusCode = string;
+    message = temp.toString();
+
     super(message);
 
     this.statusCode = statusCode;
